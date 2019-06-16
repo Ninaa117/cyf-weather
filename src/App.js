14b4dc51392737f60b0d0a94f52fe98b81d50799
Nina <ninaadam117@gmail.com>
2019-06-16 14:03:10 +0100

    all images of weather and temprature added!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import storm from "./img/weather-icons/storm.svg";
+import clear from "./img/weather-icons/clear.svg";
 
 class App extends Component {
   constructor(props) {
@@ -20,10 +21,47 @@ class App extends Component {
           </div>
 
           <div className="mainDisplay">
-            <img src={storm} alt="storm icon" className="centerImage"/>
+            <img src={storm} alt="storm icon" className="centerImage" />
             <p>Overcast clouds</p>
-            <p>Humidity 78%</p>
-            <p>More stuff</p>
+            <p>Temperature 10' to 11C</p>
+            <p>Humidity 78% Pressure 1008.48</p>
+            <div className="bottomRow">
+              <div>
+                <p>03:00 </p>
+                <img src={clear} alt="clear icon" />
+                <p>8C</p>
+              </div>
+              <div>
+                <p>06:00 </p>
+                <img src={clear} alt="clear icon" />
+                <p>9C</p>
+              </div>
+              <div>
+                <p>09:00 </p>
+                <img src={clear} alt="clear icon" />
+                <p>14C</p>
+              </div>
+              <div>
+                <p>12:00 </p>
+                <img src={clear} alt="clear icon" />
+                <p>17C</p>
+              </div>
+              <div>
+                <p>15:00 </p>
+                <img src={clear} alt="clear icon" />
+                <p>18C</p>
+              </div>
+              <div>
+                <p>18:00 </p>
+                <img src={clear} alt="clear icon" />
+                <p>16C</p>
+              </div>
+              <div>
+                <p>21:00 </p>
+                <img src={clear} alt="clear icon" />
+                <p>13C</p>
+              </div>
+            </div>
           </div>
         </main>
       </div>
